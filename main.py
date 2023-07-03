a6d3516f90a5703985b8d3089fa54cf49f5e83f1
abetoluwani <101352514+cybertolu@users.noreply.github.com>
2023-07-03 13:53:48 +0100

    on colos

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+def main ():
+    height = int(input("Height:"))
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(n):
+        print("#"*(i+1)) # the expression (i) is to add one to 1 so as to make sure the pyramid doesn't start from zero
+
+main()
+
 name = input("What is your name: ")
 
-# removing whitespace from user's by  makng it look
+# stops the compiler from stopping after the first whitespace in a string input
 name = name.strip()
 
 print(f"Hello {name}, it was nice meeting you ")
