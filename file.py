faadb6ca6ebdc2dd534a794b48b973f977a5f554
abetoluwani <101352514+cybertolu@users.noreply.github.com>
2023-07-11 14:38:07 +0000

    on colos

--- file.py
+++ file.py
@@ -1,21 +1,38 @@
-# File i/o
+# File i/o is a method used to store names instead of typing it continually
 
-def main():
-    #name()
-    nameloop()
-def collname():
-    nam = input("What's your name: ")
-    print(f"hello,{nam}")
+# def main():
+#     nameloop()
+#
+# def nameloop():
+#     names = [] # empty list
+#
+#     for i in range(3):
+#         # shorter and neater form to append multiple input
+#         names.append(input("What is your name: "))
+#
+#
+# # using the sorted funtion to sort the names alphabetically
+#     for name in sorted(names):
+#         print(f"Hello, {name}")
+#
+# if __name__ == '__main__':
+#     main()
 
-def nameloop():
-    names = [] # empty list
+# File In Python
+name = input("What is your name: ")
 
-    for i in range(3):
-        name = input("What is your name: ")
-        names.append(name)
+# file = open("names.txt","a")
+# file.write(f"{name}\n")
+# file.close()
 
-    print(names)
+# An alternative and pythonic way to the code above because it automatically close the file
+with open("names.txt","a") as file :
+    file.write(f"{name}\n")
 
-if __name__ == '__main__':
-    main()
+with open("names.txt","r") as files:
+    lines = files.readlines()
+    line = sorted(lines) # sorting what's in the file before iterating over it
 
+for j in line:
+    # print("Hello,",line,end="") # First Method
+    print("Hello,",j.rstrip()) #2nd rstrip() would strip off all new lines in the file
